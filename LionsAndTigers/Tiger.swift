c17d8bc0029dfe1f11a945de37f991d7c58a85e1
Eric Wilson <eric@triadicclosure.com>
2015-03-06 11:29:16 -0800

    adding in images

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Eric Wilson on 3/1/15.
+//  Copyright (c) 2015 Eric Wilson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+}
